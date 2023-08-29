30efa952154189bc4b2ccb901a711c4ca0ef4fcd
Sajal Gupta <sajalgupta4me@gmail.com>
2023-08-29 12:43:06 +0530

    define `NetworkError enum` and added common network error cases

--- RandomGeekJoke/RandomGeekJoke/Network/NetworkError.swift
+++ RandomGeekJoke/RandomGeekJoke/Network/NetworkError.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+enum NetworkError: Error, Equatable {
+    case noInternet
+    case pageNotFound
+    case underMaintenance
+    case invalidURL
+    case invalidData
+    case decodableFail(String)
+    case dataTaskError(String)
+    case custom(String)
+   
+    internal var message: String {
+        switch self {
+        case .noInternet:
+            return "Please try again !."
+        case .pageNotFound:
+            return "Please try again !."
+        case .underMaintenance, .invalidData:
+            return "Please try again !."
+        case .invalidURL:
+            return "We are not able to create the url, seems you have put wring parameter"
+        case let .decodableFail(errorMessage):
+            return errorMessage
+        case let .dataTaskError(errorMessage):
+            return errorMessage
+        case let .custom(errorMessage):
+            return errorMessage
+        }
+    }
+}
+
+extension NetworkError: LocalizedError {
+    var errorDescription: String? {
+        message
+    }
+}
