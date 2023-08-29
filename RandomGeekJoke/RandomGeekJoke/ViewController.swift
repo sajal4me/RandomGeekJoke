75dc1fb725f5caa5818c7c7183959a9e99296793
Sajal Gupta <sajalgupta4me@gmail.com>
2023-08-29 12:25:39 +0530

    create new project template

---
+++ RandomGeekJoke/RandomGeekJoke/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomGeekJoke
+//
+//  Created by Sajal Gupta on 29/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
