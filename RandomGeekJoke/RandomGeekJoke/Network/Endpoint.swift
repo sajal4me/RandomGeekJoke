d1a4ed290b367d8c736e9ef096b5da420d1d6f5c
Sajal Gupta <sajalgupta4me@gmail.com>
2023-08-29 12:37:44 +0530

    define endpoint protocol network properties

--- RandomGeekJoke/RandomGeekJoke/Network/Endpoint.swift
+++ RandomGeekJoke/RandomGeekJoke/Network/Endpoint.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+internal protocol Endpoint {
+    
+    var scheme: String { get }
+    
+    var baseURL: String { get }
+    
+    var path: String { get }
+    
+    var parameters: [URLQueryItem] { get }
+    
+    var method: String { get }
+}
