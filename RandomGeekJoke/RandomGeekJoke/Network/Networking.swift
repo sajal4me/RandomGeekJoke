43d1dc248c5436c7783aeb8ed2f5197b32d178dd
Sajal Gupta <sajalgupta4me@gmail.com>
2023-08-29 13:08:43 +0530

    create `generic networking protocol and Networking class` to handle all network related operation

--- RandomGeekJoke/RandomGeekJoke/Network/Networking.swift
+++ RandomGeekJoke/RandomGeekJoke/Network/Networking.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+
+protocol NetworkProtocol {
+    func request<T: Decodable>(endpoint: Endpoint, decoder: JSONDecoder) async throws -> T
+}
+
+internal final class Networking: NetworkProtocol {
+    
+    func request<T: Decodable>(endpoint: Endpoint, decoder: JSONDecoder) async throws -> T {
+        var component = URLComponents()
+        component.scheme = endpoint.scheme
+        component.host = endpoint.baseURL
+        component.path = endpoint.path
+        component.queryItems = endpoint.parameters
+        
+        guard let url = component.url else {
+            throw NetworkError.invalidURL
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = endpoint.method
+        let session = URLSession(configuration: .default)
+        
+        do  {
+            let (data, response) = try await session.data(from: url)
+            guard let httpResponse = response as? HTTPURLResponse, isOK(httpResponse) else {
+                throw NetworkError.invalidData
+            }
+            
+            return try decoder.decode(T.self, from: data)
+        } catch {
+            print(error)
+            throw NetworkError.decodableFail(error.localizedDescription)
+        }
+    }
+    
+    private func isOK(_ response: HTTPURLResponse) -> Bool {
+        (200...299).contains(response.statusCode)
+    }
+}
+
+
