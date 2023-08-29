0e32cca7ed5f453d69971491abdc12b032ed6ba3
Sajal Gupta <sajalgupta4me@gmail.com>
2023-08-29 12:39:46 +0530

    deifne `Target enum` and implement Endpoint protocol

--- RandomGeekJoke/RandomGeekJoke/Network/Target.swift
+++ RandomGeekJoke/RandomGeekJoke/Network/Target.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+
+internal enum Target {
+    case joke
+}
+
+extension Target: Endpoint {
+    enum Method: String {
+        case get = "GET"
+    }
+    
+    var scheme: String {
+        return "https"
+    }
+    
+    var baseURL: String{
+        return "geek-jokes.sameerkumar.website"
+    }
+    
+    var path: String {
+        switch self {
+        case .joke:
+            return "/api"
+        }
+    }
+    
+    var parameters: [URLQueryItem] {
+        let param = [String: String]()
+        return param.map {
+            URLQueryItem(name: $0.0, value: $0.1)
+        }
+    }
+    
+    var method: String {
+        switch self {
+        case .joke:
+            return Method.get.rawValue
+        }
+    }
+}
